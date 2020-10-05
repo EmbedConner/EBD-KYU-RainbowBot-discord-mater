3476d2f2a8250c35decd96e63718d0b61d75694f
EmbedConner Code <48331840+EmbedConner@users.noreply.github.com>
2020-10-06 03:18:22 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ exports.run = async (bot) => {
     console.log("Loading..");
     console.log(`${bot.user.tag} is online`);
     console.log("Done Rainbow..");
-    bot.user.setActivity(`Rainbow Bot|@mention me!!`)
+    bot.user.setActivity(`Rainbow Bot`)
 }
